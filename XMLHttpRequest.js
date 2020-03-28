2ad217ff1b1587146effc908576e976f2acc2e08
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2020-03-29 01:18:04 +0300

    Access data in the response

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -6,6 +6,9 @@ request.onload = function() {
     var response = request.response;
     var parsedData = JSON.parse(response);
     console.log(parsedData);
+
+    var description = parsedData[0].description;
+    console.log(description);
 }
 
 request.send();
